c4d4095964b88a6a70110ad46d2b89e0ed88d977
Du <129742087+dqswordman@users.noreply.github.com>
2024-12-02 15:35:38 +0700

    Update car_ optimization.cpp

--- car_ optimization.cpp
+++ car_ optimization.cpp
@@ -2,18 +2,18 @@ void *command(void *param){
     while(running){
         if(carControl.calibrate>=0){
             // 所有速度值增加20%
-            pwmA=((int)carStatus.speed*130)/100-(int)(carStatus.direction); 
-            pwmB=((int)carStatus.speed*120)/100+(int)(carStatus.direction);
+            pwmA=((int)carStatus.speed*200)/100-(int)(carStatus.direction);
+            pwmB=((int)carStatus.speed*160)/100+(int)(carStatus.direction);
         }else{
-            pwmA=(int)(carStatus.speed*130/100)-(int)(carStatus.direction);
-            pwmB=(int)(carStatus.speed*120/100)+(int)(carStatus.direction);
+            pwmA=(int)(carStatus.speed*200/100)-(int)(carStatus.direction);
+            pwmB=(int)(carStatus.speed*160/100)+(int)(carStatus.direction);
         }
-        
+
         // PWM限制调高
-        if(pwmA > 130) pwmA = 130;  // 允许更高的最大值
-        if(pwmB > 120) pwmB = 120;
-        
-        usleep(20000);  // 减小延迟提高响应速度
+        if(pwmA > 200) pwmA = 200;  // 允许更高的最大值
+        if(pwmB > 160) pwmB = 160;
+
+        usleep(25000);  // 减小延迟提高响应速度
     }
     pthread_exit(NULL);
 }
